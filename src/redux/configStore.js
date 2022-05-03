b5fcb115dabdeaa5232ed35ad39ba6139b43c6c5
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2022-05-03 16:55:06 +0700

    Chức năng api khoá học
    Binding lấy danh sách khoá học ở trang chủ

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,10 +1,12 @@
 
 import {combineReducers} from 'redux'
 import ChiTietKhoaHocReducer from './reducers/ChiTietKhoaHocReducer';
+import QuanLyKhoaHocReducer from './reducers/QuanLyKhoaHocReducer';
 import { QuanLyNguoiDungReducer } from './reducers/QuanLyNguoiDungReducer';
 export const rootReducer = combineReducers({
     //state ứng dụng
     QuanLyNguoiDungReducer,
-    ChiTietKhoaHocReducer
+    ChiTietKhoaHocReducer,
+    QuanLyKhoaHocReducer
 })
 
