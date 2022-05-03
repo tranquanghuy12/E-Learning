045d4df378bce60aaf11cd8743eb296de46d0f8c
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2022-05-03 14:48:23 +0700

    cập nhật UI thông tin người dùng

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,11 +1,9 @@
-import { result } from "lodash";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import swal from "sweetalert";
-import LoadingLazy from "../../components/LazyLoad/LazyLoad";
 import ModalCapNhatNguoiDung from "../../components/Modal/ModalCapNhatNguoiDung";
-
 
 import {
   huyGhiDanhKhoaHoc,
@@ -19,7 +17,6 @@ export default function Profile() {
     (rootReducer) => rootReducer.QuanLyNguoiDungReducer
   );
   let chiTietKhoaHocDaDangKy = userProfile.chiTietKhoaHocGhiDanh;
-  
 
   useEffect(() => {
     //Gọi api lấy thông tin người dùng để load lên redux
@@ -75,54 +72,68 @@ export default function Profile() {
     return <Redirect to="/login" />;
   }
   return (
-    <div>
-      <div>
-        {!userProfile ? (
-          <div>
-            <LoadingLazy />
+    <div className="container rounded bg-white mt-5 mb-5">
+      <h3 className="text-center">Thông tin tài khoản</h3>
+      <div className="row mt-5">
+        <div className="col-md-4 border-right">
+          <div className="d-flex flex-column align-items-center p-3 py-5">
+            <img
+              src="https://picsum.photos/200"
+              alt="ảnh đại diện"
+              className="rounded-circle mt-5"
+            />
+            <span className="font-weight-bold">{userProfile.taiKhoan}</span>
+            <span className="text-black-50">{userProfile.email}</span>
+            <span></span>
           </div>
-        ) : (
-          <div className="container">
-            <div className="form-group">
-              <h5>Tài khoản</h5>
-              <p>{userProfile.taiKhoan}</p>
-            </div>
-            <div className="form-group">
-              <h5>Họ Tên</h5>
-              <p>{userProfile.hoTen}</p>
-            </div>
-            <div className="form-group">
-              <h5>Email</h5>
-              <p>{userProfile.email}</p>
-            </div>
-            <div className="form-group">
-              <h5>Số điện thoại</h5>
-              <p>{userProfile.soDT}</p>
-            </div>
-            <div className="form-group">
-              <h5>Mã nhóm</h5>
-              <p>{userProfile.maNhom}</p>
-            </div>
-            <div className="form-group">
-              <h5>Mã loại người dùng</h5>
-              <p>{userProfile.maLoaiNguoiDung}</p>
-            </div>
-            <div className="d-flex justify-content-center">
-            
-              <ModalCapNhatNguoiDung userProfile={userProfile}/>              
-              <button className="ml-3 btn btn-danger">Rời khỏi</button>
-            </div>
-            <h1 className="">Khoá học đã đăng ký</h1>
-            <div>
-              {!userProfile.chiTietKhoaHocGhiDanh ? (
-                <h3>Bạn chưa đăng ký khoá học nào</h3>
-              ) : (
-                <div className="row">{khoaHocDaDangKy()}</div>
-              )}
-            </div>
+        </div>
+        <div className="col-md-8">
+          <div className="form-group">
+            <h5>Tài khoản</h5>
+            <p>{userProfile.taiKhoan}</p>
           </div>
-        )}
-      </div>     
+          <div className="form-group border-top">
+            <h5 className="mt-2">Họ Tên</h5>
+            <p>{userProfile.hoTen}</p>
+          </div>
+          <div className="form-group border-top">
+            <h5 className="mt-2">Email</h5>
+            <p>{userProfile.email}</p>
+          </div>
+          <div className="form-group border-top">
+            <h5 className="mt-2">Số điện thoại</h5>
+            <p>{userProfile.soDT}</p>
+          </div>
+          <div className="form-group border-top">
+            <h5 className="mt-2">Mã nhóm</h5>
+            <p>{userProfile.maNhom}</p>
+          </div>
+          <div className="form-group border-top">
+            <h5 className="mt-2">Mã loại người dùng</h5>
+            <p>{userProfile.maLoaiNguoiDung}</p>
+          </div>
+          <div className="d-flex justify-content-center">
+            <ModalCapNhatNguoiDung userProfile={userProfile} />
+            <NavLink className="ml-3 btn btn-danger" to="/">
+              Rời khỏi
+            </NavLink>
+          </div>
+        </div>
+      </div>
+
+      {/* Khoá học đã đăng ký */}
+      <div className="row mt-5">
+        <div className="col-md-12">
+          <h3 className="text-center">Khoá học đã đăng ký</h3>
+          <div className="mt-5">
+            {!userProfile.chiTietKhoaHocGhiDanh ? (
+              <h3>Bạn chưa đăng ký khoá học nào</h3>
+            ) : (
+              <div className="row">{khoaHocDaDangKy()}</div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
