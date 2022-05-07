ea7ae3bfb1289271ee9fe6da0cb6871191bfe3fd
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2022-05-07 11:59:49 +0700

    fixed  chuc nang huy ghi danh khoa hoc

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,12 +1,18 @@
-
-import {combineReducers} from 'redux'
-import ChiTietKhoaHocReducer from './reducers/ChiTietKhoaHocReducer';
-import QuanLyKhoaHocReducer from './reducers/QuanLyKhoaHocReducer';
-import { QuanLyNguoiDungReducer } from './reducers/QuanLyNguoiDungReducer';
+import { applyMiddleware, combineReducers, createStore, compose } from "redux";
+import ChiTietKhoaHocReducer from "./reducers/ChiTietKhoaHocReducer";
+import QuanLyKhoaHocReducer from "./reducers/QuanLyKhoaHocReducer";
+import { QuanLyNguoiDungReducer } from "./reducers/QuanLyNguoiDungReducer";
+import reduxThunk from "redux-thunk";
 export const rootReducer = combineReducers({
-    //state ứng dụng
-    QuanLyNguoiDungReducer,
-    ChiTietKhoaHocReducer,
-    QuanLyKhoaHocReducer
-})
+  //state ứng dụng
+  QuanLyNguoiDungReducer,
+  ChiTietKhoaHocReducer,
+  QuanLyKhoaHocReducer,
+});
 
+let middleWare = applyMiddleware(reduxThunk);
+let composeCustom = compose(
+  middleWare,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export const store = createStore(rootReducer, composeCustom);
--- src/redux/reducers/ChiTietKhoaHocReducer.js
+++ src/redux/reducers/ChiTietKhoaHocReducer.js
@@ -1,23 +1,21 @@
 import {
+  CAP_NHAT_KHOA_HOC,
   CHI_TIET_KHOA_HOC_DA_DANG_KY,
-  XOA_KHOA_HOC_DA_DANG_KY,
 } from "../types/QuanLyKhoaHocType";
 
 let stateDefault = [];
 
 const ChiTietKhoaHocReducer = (state = stateDefault, action) => {
   switch (action.type) {
-    case XOA_KHOA_HOC_DA_DANG_KY:
-      let mangKhoaHoc = [...state];
-      console.log('mangKhoaHoc',mangKhoaHoc)
-      const index = mangKhoaHoc.findIndex(
-        (data) => data.maKhoaHoc === action.payload
-      );
-      console.log("index", index);
-      mangKhoaHoc.splice(index, 1);
-      state = mangKhoaHoc;
-      console.log("state reducer", state);
+    case CAP_NHAT_KHOA_HOC:
+      let newList = [...state];
+      let index = newList.findIndex(
+        (item) => item.maKhoaHoc === action.payload
+      );      
+      newList.splice(index, 1);
+      state = newList;      
       return [...state];
+
     case CHI_TIET_KHOA_HOC_DA_DANG_KY:
       state = action.payload;
       return { ...state };
