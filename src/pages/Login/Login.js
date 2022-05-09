98aa54743bdc24f3a2daeeb519e67cc141b60999
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2022-05-09 16:59:08 +0700

    sua loi css anh huong den global

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -23,67 +23,69 @@ export default function Login() {
   });
 
   return (
-    <div className="container">
-      <div className={`row content ${styles.content}`}>
-        <div className="col-md-6 mb-3">
-          <img src={loginSvg} className="img-fluid" alt="image" />
-        </div>
-        <div className="col-md-6">
-          <h3 className={`${styles.signinText} mb-3`}>Đăng nhập</h3>
-          <form onSubmit={formik.handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="taiKhoan">Tài khoản</label>
-              <input
-                className={`form-control ${styles.formControl}`}
-                type="text"
-                name="taiKhoan"
-                onChange={formik.handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Mật khẩu</label>
-              <input
-                className={`form-control ${styles.formControl}`}
-                type="password"
-                name="matKhau"
-                onChange={formik.handleChange}
-              />
-            </div>
-            <div className="form-group form-check">
-              <input
-                type="checkbox"
-                name="checkbox"
-                className="form-check-input"
-                id="checkbox"
-              />
-              <label className="form-check-label" htmlFor="checkbox">
-                Duy trì đăng nhập
-              </label>
-            </div>
-            <p className="text-center">
-              Nếu bạn quên mật khẩu. <a href="http://">Nhấp vào đây</a>
-            </p>
-            <div className="row text-center mt-5">
-              <div className="col-6">
-                <button
-                  type="submit"
-                  className={`btn btn-class ${styles.btnClassLogin}`}
-                >
-                  Tiếp theo
-                </button>
+    <>
+      <div className="container">
+        <div className={`row ${styles.contentLogin}`}>
+          <div className="col-md-6 mb-3">
+            <img src={loginSvg} className="img-fluid" alt="image" />
+          </div>
+          <div className="col-md-6">
+            <h3 className={`${styles.signinText} mb-3`}>Đăng nhập</h3>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="taiKhoan">Tài khoản</label>
+                <input
+                  className={`form-control ${styles.formControl}`}
+                  type="text"
+                  name="taiKhoan"
+                  onChange={formik.handleChange}
+                />
               </div>
-              <div className="col-6">
-                <button
-                  type="submit"
-                  className={`btn btn-class ${styles.btnClass}`}
-                >
-                  Đăng ký
-                </button>
+              <div className="form-group">
+                <label htmlFor="password">Mật khẩu</label>
+                <input
+                  className={`form-control ${styles.formControl}`}
+                  type="password"
+                  name="matKhau"
+                  onChange={formik.handleChange}
+                />
               </div>
-            </div>
-          </form>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  className="form-check-input"
+                  id="checkbox"
+                />
+                <label className="form-check-label" htmlFor="checkbox">
+                  Duy trì đăng nhập
+                </label>
+              </div>
+              <p className="text-center">
+                Nếu bạn quên mật khẩu. <a href="http://">Nhấp vào đây</a>
+              </p>
+              <div className="row text-center mt-5">
+                <div className="col-6">
+                  <button
+                    type="submit"
+                    className={`btn btn-class ${styles.btnClassLogin}`}
+                  >
+                    Tiếp theo
+                  </button>
+                </div>
+                <div className="col-6">
+                  <button
+                    type="submit"
+                    className={`btn btn-class ${styles.btnClass}`}
+                  >
+                    Đăng ký
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -8,128 +8,133 @@ import signUp from "../../assets/img/signup.jpg";
 import styles from "../Register/Register.module.css";
 
 export default function Register(props) {
-  useEffect(()=>{
-    document.body.style.backgroundColor="#fec107"
-    document.body.style.fontFamily="Roboto,sans-serif"
-  },[]);
+  useEffect(() => {
+    document.body.style.backgroundColor = "#fec107";
+    document.body.style.fontFamily = "Roboto,sans-serif";
+  }, []);
   const dispatch = useDispatch();
   return (
-    <section className="vh-100 mx-auto">
-      <div className="container">
-        <div className={`row content align-items-center ${styles.content}`}>
-          <div className="col-md-6 mb-3 ">
-            <img src={signUp} className="img-fluid" alt="đăng ký" />
-          </div>
-          <div className="col-md-6">
-            <h1 className={`${styles.signupText} text-center`}>Đăng ký</h1>
-            <Formik
-              initialValues={{
-                email: "",
-                hoTen: "",
-                maNhom: "GP01",
-                soDT: "",
-                taiKhoan: "",
-                matKhau: "",
-              }}
-              validationSchema={DangKySchema}
-              onSubmit={(values) => {
-                const action = dangKyAction(values);
-                dispatch(action);
-              }}
-              render={(formikProps) => (
-                <Form>
-                  <div className="form-group">
-                    <label>Tài khoản</label>
-                    <Field
-                      type="text"
-                      className={`form-control ${styles.formControl}`}
-                      name="taiKhoan"
-                      onChange={formikProps.handleChange}
-                    />
-                    <ErrorMessage name="taiKhoan" />
-                  </div>
-                  <div className="form-group">
-                    <label>Mật khẩu</label>
-                    <Field
-                      className={`form-control ${styles.formControl}`}
-                      name="matKhau"
-                      type="password"
-                      onChange={formikProps.handleChange}
-                    />
-                    <ErrorMessage name="matKhau" />
-                  </div>
-                  <div className="form-group">
-                    <label>Email</label>
-                    <Field
-                      className={`form-control ${styles.formControl}`}
-                      name="email"
-                      type="email"
-                      onChange={formikProps.handleChange}
-                    />
-                    <ErrorMessage name="email" />
-                  </div>
-                  <div className="form-group">
-                    <label>Họ tên: </label>
-                    <Field
-                      placeholder="Nhập họ tên"
-                      className={`form-control ${styles.formControl}`}
-                      name="hoTen"
-                      onChange={formikProps.handleChange}
-                    />
-                    <ErrorMessage name="hoTen" />
-                  </div>
-                  <div className="form-group">
-                    <label>Số điện thoại: </label>
-                    <Field
-                      placeholder="Số điện thoại"
-                      className={`form-control ${styles.formControl}`}
-                      name="soDT"
-                      onChange={formikProps.handleChange}
-                    />
-                    <ErrorMessage name="soDT" />
-                  </div>
-                  <div className="form-group">
-                    <label>Mã nhóm: </label>
-                    <Field
-                      component="select"
-                      name="maNhom"
-                      className={`form-control ${styles.formControl}`}
-                      onChange={formikProps.handleChange}
-                    >
-                      <option>GP01</option>
-                      <option>GP02</option>
-                      <option>GP03</option>
-                      <option>GP04</option>
-                      <option>GP05</option>
-                      <option>GP06</option>
-                      <option>GP07</option>
-                      <option>GP08</option>
-                      <option>GP09</option>
-                      <option>GP10</option>
-                    </Field>
-                    <ErrorMessage name="maNhom" />
-
-                    <div className="d-flex justify-content-center mt-5">
-                      <button
-                        className={`btn ${styles.btnClassReturn} mr-2 `}
-                        type="submit"
-                      >
-                        Trở về
-                      </button>
-                      <button
-                        className={`btn ${styles.btnClass}`}
-                        type="submit"
-                      >
-                        Đăng ký
-                      </button>
+    <>
+      {/* <Helmet>
+        <body></body>
+      </Helmet> */}
+      <div className="vh-100 mx-auto">
+        <div className="container">
+          <div className={`row align-items-center ${styles.contentRegister}`}>
+            <div className="col-md-6 mb-3 ">
+              <img src={signUp} className="img-fluid" alt="đăng ký" />
+            </div>
+            <div className="col-md-6">
+              <h1 className={`${styles.signupText} text-center`}>Đăng ký</h1>
+              <Formik
+                initialValues={{
+                  email: "",
+                  hoTen: "",
+                  maNhom: "GP01",
+                  soDT: "",
+                  taiKhoan: "",
+                  matKhau: "",
+                }}
+                validationSchema={DangKySchema}
+                onSubmit={(values) => {
+                  const action = dangKyAction(values);
+                  dispatch(action);
+                }}
+                render={(formikProps) => (
+                  <Form>
+                    <div className="form-group">
+                      <label>Tài khoản</label>
+                      <Field
+                        type="text"
+                        className={`form-control ${styles.formControl}`}
+                        name="taiKhoan"
+                        onChange={formikProps.handleChange}
+                      />
+                      <ErrorMessage name="taiKhoan" />
                     </div>
-                  </div>
-                </Form>
-              )}
-            />
+                    <div className="form-group">
+                      <label>Mật khẩu</label>
+                      <Field
+                        className={`form-control ${styles.formControl}`}
+                        name="matKhau"
+                        type="password"
+                        onChange={formikProps.handleChange}
+                      />
+                      <ErrorMessage name="matKhau" />
+                    </div>
+                    <div className="form-group">
+                      <label>Email</label>
+                      <Field
+                        className={`form-control ${styles.formControl}`}
+                        name="email"
+                        type="email"
+                        onChange={formikProps.handleChange}
+                      />
+                      <ErrorMessage name="email" />
+                    </div>
+                    <div className="form-group">
+                      <label>Họ tên: </label>
+                      <Field
+                        placeholder="Nhập họ tên"
+                        className={`form-control ${styles.formControl}`}
+                        name="hoTen"
+                        onChange={formikProps.handleChange}
+                      />
+                      <ErrorMessage name="hoTen" />
+                    </div>
+                    <div className="form-group">
+                      <label>Số điện thoại: </label>
+                      <Field
+                        placeholder="Số điện thoại"
+                        className={`form-control ${styles.formControl}`}
+                        name="soDT"
+                        onChange={formikProps.handleChange}
+                      />
+                      <ErrorMessage name="soDT" />
+                    </div>
+                    <div className="form-group">
+                      <label>Mã nhóm: </label>
+                      <Field
+                        component="select"
+                        name="maNhom"
+                        className={`form-control ${styles.formControl}`}
+                        onChange={formikProps.handleChange}
+                      >
+                        <option>GP01</option>
+                        <option>GP02</option>
+                        <option>GP03</option>
+                        <option>GP04</option>
+                        <option>GP05</option>
+                        <option>GP06</option>
+                        <option>GP07</option>
+                        <option>GP08</option>
+                        <option>GP09</option>
+                        <option>GP10</option>
+                      </Field>
+                      <ErrorMessage name="maNhom" />
+
+                      <div className="d-flex justify-content-center mt-5">
+                        <button
+                          className={`btn ${styles.btnClassReturn} mr-2 `}
+                          type="submit"
+                        >
+                          Trở về
+                        </button>
+                        <button
+                          className={`btn ${styles.btnClass}`}
+                          type="submit"
+                        >
+                          Đăng ký
+                        </button>
+                      </div>
+                    </div>
+                  </Form>
+                )}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </section>
+    </>
   );
 }
