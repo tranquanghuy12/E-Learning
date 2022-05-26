c8e010aeb57978b3b5acf02184ee75b2ad05b6ea
Silver Wolf <84911530+Khoa-sudo@users.noreply.github.com>
2022-05-26 10:16:22 +0700

    Pagination cho trang home
    sử dụng api "LayDanhSachKhoaHoc_PhanTrang?"

--- src/redux/actions/QuanLyKhoaHocAction.js
+++ src/redux/actions/QuanLyKhoaHocAction.js
@@ -1,7 +1,12 @@
 import { createAction } from ".";
-import { API_TIM_KIEM_KHOA_HOC, http } from "../../util/setting/config";
+import {
+  API_LAY_DANH_SACH_KHOA_HOC_PHAN_TRANG,
+  API_TIM_KIEM_KHOA_HOC,
+  http,
+} from "../../util/setting/config";
 import {
   LAY_DANH_SACH_KHOA_HOC_ACTION,
+  LAY_DS_KHOA_HOC_PHAN_TRANG_ACTION,
   TIM_KIEM_KHOA_HOC_ACTION,
 } from "../types/QuanLyKhoaHocType";
 
@@ -23,6 +28,19 @@ export function timKiemTenKhoaHocAction(text) {
     try {
       let result = await http.get(`${API_TIM_KIEM_KHOA_HOC}${text}`);
       dispatch(createAction(TIM_KIEM_KHOA_HOC_ACTION, result.data));
+    } catch (error) {
+      console.log(error.response?.data);
+    }
+  };
+}
+
+export function layDanhSachKhoaHocPhanTrang(params) {
+  return async (dispatch) => {
+    try {
+      let result = await http.get(
+        `${API_LAY_DANH_SACH_KHOA_HOC_PHAN_TRANG}${params}`
+      );
+      dispatch(createAction(LAY_DS_KHOA_HOC_PHAN_TRANG_ACTION, result.data));
     } catch (error) {
       console.log(error.response?.data);
     }
--- src/util/setting/config.js
+++ src/util/setting/config.js
@@ -27,6 +27,7 @@ export const API_HUYGHIDANH_KHOAHOC = DOMAIN + API_KHOAHOC + "HuyGhiDanh";
 export const API_TIM_KIEM_KHOA_HOC =
   DOMAIN + API_KHOAHOC + "LayDanhSachKhoaHoc?tenKhoaHoc=";
 export const API_THEM_KHOA_HOC_ADMIN = DOMAIN + API_KHOAHOC + "ThemKhoaHoc";
+export const API_LAY_DANH_SACH_KHOA_HOC_PHAN_TRANG = DOMAIN + API_KHOAHOC + 'LayDanhSachKhoaHoc_PhanTrang?'
 //API Admin
 export const API_LAY_DANH_SACH_NGUOI_DUNG =
   DOMAIN + API_NGUOIDUNG + "LayDanhSachNguoiDung?";
