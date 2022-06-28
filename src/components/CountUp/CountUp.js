358c2b3c23f362542141b328f1b88c7117debaa4
Huy Tran <huy.b.tran@capgemini.com>
2022-06-28 17:55:30 +0700

    update responsive Home component

--- src/components/CountUp/CountUp.js
+++ src/components/CountUp/CountUp.js
@@ -5,15 +5,15 @@ export default function CountUp() {
   return (
     <div className="count mt-5">
       <div className="count__content row m-0">
-        <div className="count__item col-sm-12 col-md-3 col-lg-3">
-          <i className="icon style__icon fas fa-smile"></i>
+        <div className="count__item col-sm-6 col-md-3 col-lg-3">
+          <i className="icon fas fa-smile"></i>
           <h2 className="count__title">
             <CountUpReact end={18} duration={2} />
             Ml
           </h2>
           <p className="style__description">Học viên hài lòng</p>
         </div>
-        <div className="count__item col-sm-12 col-md-3 col-lg-3">
+        <div className="count__item col-sm-6 col-md-3 col-lg-3">
           <i className="icon style__icon fas fa-chart-line"></i>
           <h2 className="count__title">
             <CountUpReact end={20} duration={2} />
@@ -21,7 +21,7 @@ export default function CountUp() {
           </h2>
           <p className="style__description">Dự án tốt nghiệp</p>
         </div>
-        <div className="count__item col-sm-12 col-md-3 col-lg-3">
+        <div className="count__item col-sm-6 col-md-3 col-lg-3">
           <i className="icon style__icon fas fa-rocket"></i>
           <h2 className="count__title">
             <CountUpReact end={30} duration={2} />
@@ -29,7 +29,7 @@ export default function CountUp() {
           </h2>
           <p className="style__description">Dự án phát triển</p>
         </div>
-        <div className="count__item col-sm-12 col-md-3 col-lg-3">
+        <div className="count__item col-sm-6 col-md-3 col-lg-3">
           <i className="icon style__icon fa fa-birthday-cake"></i>
           <h2 className="count__title">
             <CountUpReact end={50} duration={2} />
